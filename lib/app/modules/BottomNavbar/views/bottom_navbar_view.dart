6ba2afefeafc6863735a1b01dc0d8b5979859210
Paul Jeconiah <pauljack1672@gmail.com>
2025-03-07 00:55:38 +0700

    Mengedit pada bagian BottomNavbar dengan mengganti BottomNavigationBar dengan Gnav

--- lib/app/modules/BottomNavbar/views/bottom_navbar_view.dart
+++ lib/app/modules/BottomNavbar/views/bottom_navbar_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:project_akhir_flutter_bootcamp/app/modules/AkunSaya/views/akun_saya_view.dart';
 import 'package:project_akhir_flutter_bootcamp/app/modules/BottomNavbar/controllers/bottom_navbar_controller.dart';
 import 'package:project_akhir_flutter_bootcamp/app/modules/berita/views/berita_view.dart';
@@ -16,27 +17,37 @@ class BottomNavbarView extends GetView<BottomNavbarController> {
       body: Obx(() => _pages[controller.selectedIndex.value]),
       bottomNavigationBar: SafeArea(
         child: Obx(
-          () => BottomNavigationBar(
-            backgroundColor: Color(0xFF722F37),
-            currentIndex: controller.selectedIndex.value,
-            selectedItemColor: Colors.white,
-            unselectedItemColor: Colors.white70,
-            onTap: (value) => controller.changeIndex(value),
-            items: [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                label: "Beranda",
-                backgroundColor: Colors.white,
+          () => Container(
+            color: Color(0xFF722F37),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+              child: GNav(
+                backgroundColor: Color(0xFF722F37),
+                // currentIndex: controller.selectedIndex.value,
+                // selectedItemColor: Colors.white,
+                color: Colors.white,
+                activeColor: Colors.white,
+                tabBackgroundColor: Color(0xFF4E2328),
+                onTabChange: (value) => controller.changeIndex(value),
+                selectedIndex: controller.selectedIndex.value,
+                gap: 8,
+                padding: EdgeInsets.all(16),
+                // unselectedItemColor: Colors.white70,
+                // onTap: (value) => controller.changeIndex(value),
+                tabs: [
+                  GButton(
+                    icon: Icons.home,
+                    text: "Beranda",
+                    // backgroundColor: Colors.white,
+                  ),
+                  GButton(icon: Icons.newspaper_outlined, text: "Berita"),
+                  GButton(
+                    icon: Icons.person_outline_outlined,
+                    text: "Akun Saya",
+                  ),
+                ],
               ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.newspaper_outlined),
-                label: "Berita",
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.person_outline_outlined),
-                label: "Akun Saya",
-              ),
-            ],
+            ),
           ),
         ),
       ),
